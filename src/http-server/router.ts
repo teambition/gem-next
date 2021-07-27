f9e8fd1c015de2b3c3212d786a7fb9a873d4cbc2
Orange Mi <aliyun@miwenjie.com>
2021-07-27 15:09:54 +0000

    fix: start script

--- src/http-server/router.ts
+++ src/http-server/router.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 import * as glob from 'glob'
 import { getRouter } from './decorator'
 
-glob.sync('api/**/*.ts', {
+glob.sync('api/**/*.[jt]s', {
   cwd: path.resolve(__dirname, '../'),
 }).forEach(file => {
   require('../' + file)
